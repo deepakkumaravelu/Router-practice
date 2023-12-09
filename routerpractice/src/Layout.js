e3a723b90d7b788c0b878c11d009e0d383022ee6
deepakkumaravelu <deepak1122003kumar@gmail.com>
2023-12-09 16:02:41 +0530

    router practice

---
+++ routerpractice/src/Layout.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function Layout() {
+  return (
+    <div>
+    <nav>
+    <ul>
+    <li></li></ul>
+    </nav>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </div>
+  )
+}
+
+export default Layout
